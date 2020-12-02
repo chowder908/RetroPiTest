0b865460c6bc9bff09142309ee66f32e8096877f
chowder908 <chowder90856@gmail.com>
2020-12-01 20:54:22 -0600

    Update citra.sh

--- citra.sh
+++ citra.sh
@@ -8,9 +8,15 @@ rp_module_desc="3ds emulator"
 rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to  $romdir/3ds"
 rp_module_licence="GPL2 https://github.com/citra-emu/citra/blob/master/license.txt"
 rp_module_section="exp"
-rp_module_flags="!arm"
 
-function depends_citra() {
+function _params_lr-desmume() {
+    local params=()
+    isPlatform "arm" && params+=("platform=armvhardfloat")
+    isPlatform "aarch64" && params+=("DENABLE_QT=OFF")
+    echo "${params[@]}"
+}
+
+function depends_lr-desmume() {
     if compareVersions $__gcc_version lt 7; then
         md_ret_errors+=("Sorry, you need an OS with gcc 7.0 or newer to compile citra")
         return 1
@@ -21,40 +27,30 @@ function depends_citra() {
     getDepends "${depends[@]}"
 }
 
-function sources_citra() {
-    gitPullOrClone "$md_build" https://github.com/citra-emu/citra.git
+function sources_lr-desmume() {
+    gitPullOrClone "$md_build" https://github.com/libretro/citra.git
 }
 
-function build_citra() {
-    cd "$md_build/citra"
-    mkdir build
-    cd build
-    cmake .. -DCMAKE_BUILD_TYPE=Release
-    make
-    md_ret_require="$md_build/build/bin"
-
+function build_lr-desmume() {
+    cd desmume/src/frontend/libretro
+    make clean
+    make $(_params_lr-desmume)
+    md_ret_require="$md_build/desmume/src/frontend/libretro/citra_libretro.so"
 }
 
-function install_citra() {
-      md_ret_files=(
-      '/build/bin/citra'
-      '/build/bin/citra-qt'
-      ''
-      )
-
+function install_lr-desmume() {
+    md_ret_files=(
+        'desmume/src/frontend/libretro/citra_libretro.so'
+    )
 }
 
-function configure_citra() {
-
+function configure_lr-desmume() {
     mkRomDir "3ds"
-
     ensureSystemretroconfig "3ds"
 
-
-
-    addEmulator 0 "$md_id" "3ds" "$md_inst/citra %ROM%"
-    addEmulator 1 "$md_id-qt" "3ds" "$md_inst/citra-qt %ROM%"
+    addEmulator 0 "$md_id" "3ds" "$md_inst/citra_libretro.so"
     addSystem "3ds"
-
-
 }
+
+
+ 
