9510d60bef342584b25dfd2e907ebcda9a5e32f8
chowder908 <chowder90856@gmail.com>
2020-12-01 21:09:17 -0600

    Update citra.sh

--- citra.sh
+++ citra.sh
@@ -8,15 +8,9 @@ rp_module_desc="3ds emulator"
 rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to  $romdir/3ds"
 rp_module_licence="GPL2 https://github.com/citra-emu/citra/blob/master/license.txt"
 rp_module_section="exp"
+rp_module_flags="!arm"
 
-function _params_lr-desmume() {
-    local params=()
-    isPlatform "arm" && params+=("platform=armvhardfloat")
-    isPlatform "aarch64" && params+=("DENABLE_QT=OFF")
-    echo "${params[@]}"
-}
-
-function depends_lr-desmume() {
+function depends_citra() {
     if compareVersions $__gcc_version lt 7; then
         md_ret_errors+=("Sorry, you need an OS with gcc 7.0 or newer to compile citra")
         return 1
@@ -27,30 +21,40 @@ function depends_lr-desmume() {
     getDepends "${depends[@]}"
 }
 
-function sources_lr-desmume() {
-    gitPullOrClone "$md_build" https://github.com/libretro/citra.git
+function sources_citra() {
+    gitPullOrClone "$md_build" https://github.com/citra-emu/citra.git
 }
 
-function build_lr-desmume() {
-    cd desmume/src/frontend/libretro
-    make clean
-    make $(_params_lr-desmume)
-    md_ret_require="$md_build/desmume/src/frontend/libretro/citra_libretro.so"
+function build_citra() {
+    cd "$md_build/citra"
+    mkdir build
+    cd build
+    cmake .. -DENABLE_SDL2=0 -DENABLE_QT=0 -DCMAKE_BUILD_TYPE="Release"
+    make
+    md_ret_require="$md_build/build/bin"
+
 }
 
-function install_lr-desmume() {
-    md_ret_files=(
-        'desmume/src/frontend/libretro/citra_libretro.so'
-    )
+function install_citra() {
+      md_ret_files=(
+      '/build/bin/citra'
+      '/build/bin/citra-qt'
+      ''
+      )
+
 }
 
-function configure_lr-desmume() {
+function configure_citra() {
+
     mkRomDir "3ds"
+
     ensureSystemretroconfig "3ds"
 
-    addEmulator 0 "$md_id" "3ds" "$md_inst/citra_libretro.so"
+
+
+    addEmulator 0 "$md_id" "3ds" "$md_inst/citra %ROM%"
+    addEmulator 1 "$md_id-qt" "3ds" "$md_inst/citra-qt %ROM%"
     addSystem "3ds"
+
+
 }
-
-
- 
